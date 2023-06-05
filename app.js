143fd05e73d0dde2abe0091ee1005abf97e74106
samantha-aguirrec <123768420+samantha-aguirrec@users.noreply.github.com>
2023-06-05 12:40:48 -0500

    .....

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const cuadrado = document.querySelectorAll(".cuadrado");
 const tiempofaltante = document.querySelector("#Tiempo");
-let puntaje = document.getElementById("puntaje");
+let puntaje: = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
 
-function cuadradoazar { 
-  cuadrado.forEach(nombredeclase => { 
+function cuadradoazar() {
+  cuadrado.forEach((nombredeclase) => {
     nombredeclase.classList.remove("topo");
-     })
-     let posicionalazar = cuadrado [Math.floor (Math.randon() *9)]
-     posicionalazar.classlist.add("topo");
-     posiciontopo.posicionalazar.Id
+  });
+  let posicionalazar = cuadrado[Math.floor(Math.random() * 9)];
+  posicionalazar.classList.add("topo");
+  posiciontopo = posicionalazar.id;
 }
-cuadrado.forEach (identificador => { 
-  identificador.addEventListener("click" () => { 
-    if (identificador.Id === posiciontopo) { 
+cuadrado.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if (identificador.id === posiciontopo) {
       resultado = resultado + 1;
-      puntaje.textcontent = resultado;
+      puntaje.textContent = resultado;
       posiciontopo = null;
-     }
-     });
- });
- function movertopo (){
-   tiempotopo = setInterval (cuadradoazar,700);
- }
- movertopo();
+    }
+  });
+});
+function movertopo() {
+  tiempotopo = setInterval(cuadradoazar, 700);
+}
+movertopo();
 
- function cuentaregresiva (){
-   tiempoactual--;
-   tiempofaltante.textContent = tiempoactual;
-   if (tiempoactual===0){
-     clearInterval(idtiempo);
-     clearInterval(tiempotopo);
-    alert ("Se acabó el tiempo, tu puntaje fue" + resultado + "topos atrapados");
-   }
- }
+function cuentaregresiva() {
+  tiempoactual--;
+  tiempofaltante.textContent = tiempoactual;
+  if (tiempoactual === 0) {
+    clearInterval(idtiempo);
+    clearInterval(tiempotopo);
+    alert("Se acabó el tiempo, tu puntaje fue" + resultado + "topos atrapados");
+  }
+}
